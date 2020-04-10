3c40194dcbf42b1e16fab0bd343fa71efe15fda6
Jegan Kabilan <jegancbk@gmail.com>
2020-04-10 17:28:49 -0500

    swagger changes

--- productrestapi/src/main/java/com/jegan/springweb/ProductrestapiApplication.java
+++ productrestapi/src/main/java/com/jegan/springweb/ProductrestapiApplication.java
@@ -6,8 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 @EnableCaching
+@EnableSwagger2
 public class ProductrestapiApplication {
 
 	public static void main(String[] args) {
